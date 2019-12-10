aff2b6ea2d36e4e1edc27690fd69cdf90d62bdf4
unknown <209359@carbon.cti.p.lodz.pl>
2019-12-10 19:00:05 +0100

    Done

---
+++ src/main/java/com/konopka/demo/controller/CalendarController.java
@@ -0,0 +1,69 @@
+package com.konopka.demo;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+@RestController
+public class CalendarEndpoint {
+
+    private CalendarService calendarService;
+
+    @Autowired
+    public CalendarEndpoint(CalendarService calendarService) {
+        this.calendarService = calendarService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/calendar")
+    public ResponseEntity<Resource> fetchCalendarFileInICalFormat() throws IOException {
+        int month = Calendar.getInstance().get(Calendar.MONTH) + 1;
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        List<String> eventsInCalendar = fetchEventsFromSpecificCalendar(month, year);
+        List<String> summariesForEventsInCalendar = fetchSummariesForEventsInCalendar(month, year);
+        Resource fileSystemResource = new FileSystemResource(calendarService.createNewCalendarFile(eventsInCalendar, summariesForEventsInCalendar, month));
+
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.parseMediaType("text/calendar"))
+                .body(fileSystemResource);
+    }
+
+    private List<String> fetchSummariesForEventsInCalendar(int month, int year) throws IOException {
+        List<String> summaries = new ArrayList<>();
+        Document document = Jsoup.connect("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=" + year + "&miesiac=" + month + "&lang=1").get();
+        Elements elements = document.select("div.InnerBox");
+
+        for (Element element : elements) {
+            summaries.add(element.text());
+        }
+
+        return summaries;
+    }
+
+    private List<String> fetchEventsFromSpecificCalendar(int month, int year) throws IOException {
+        Document document = Jsoup.connect("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=" + year + "&miesiac=" + month + "&lang=1").get();
+        Elements elements = document.select("a.active");
+        List<String> events = new ArrayList<>();
+
+        for (Element element : elements) {
+            events.add(element.text());
+        }
+
+        return events;
+    }
+
+}
---
+++ src/main/java/com/konopka/demo/service/CalendarService.java
@@ -0,0 +1,49 @@
+package com.konopka.demo;
+
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class CalendarService {
+
+    public File createNewCalendarFile(List<String> dates, List<String> summaries, int month) throws IOException {
+        File file = new File(CalendarUtils.FILE_NAME);
+        FileWriter fileWriter = new FileWriter(file.getAbsoluteFile());
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+        bufferedWriter.write(CalendarUtils.CAL_BEGIN);
+        bufferedWriter.write(CalendarUtils.VERSION);
+        bufferedWriter.write(CalendarUtils.PROD_ID);
+        bufferedWriter.write(CalendarUtils.CAL_SCALE);
+        bufferedWriter.write(CalendarUtils.METHOD);
+
+        writeFile(dates, summaries, month, bufferedWriter);
+
+        bufferedWriter.write(CalendarUtils.CAL_END);
+        bufferedWriter.close();
+
+        return file;
+    }
+
+    private void writeFile(List<String> dates, List<String> summaries, int month, BufferedWriter bufferedWriter) throws IOException {
+        for (int i = 0; i < dates.size(); i++) {
+            bufferedWriter.write(CalendarUtils.EVENT_BEGIN);
+            if (dates.get(i).length() == 1) {
+                bufferedWriter.write(CalendarUtils.DATE_START + month + "0" + dates.get(i) + "\r\n");
+                bufferedWriter.write(CalendarUtils.DATE_END + month + "0" +dates.get(i) + "\r\n");
+            }
+            else {
+                bufferedWriter.write(CalendarUtils.DATE_START + month + dates.get(i) + "\r\n");
+                bufferedWriter.write(CalendarUtils.DATE_END + month +dates.get(i) + "\r\n");
+            }
+            bufferedWriter.write(CalendarUtils.SUMMARY + summaries.get(i) + "\r\n");
+            bufferedWriter.write(CalendarUtils.EVENT_END);
+        }
+    }
+
+}
